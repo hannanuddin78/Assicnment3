ba46005de2e0fa0e9f024a46af6ae2a55ca5f0a3
hannanuddin78 <67517002+hannanuddin78@users.noreply.github.com>
2020-07-17 22:24:53 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,12 @@
+//  feet To Mile start now
+
+function feetToMile(feet) {
+    var mile = feet / 5280.0;
+    return mile;
+}
+// 5280.0 ft = 1 mile
+var result = feetToMile(1000);
+console.log(result);
+
+//  feet To Mile start End
+
